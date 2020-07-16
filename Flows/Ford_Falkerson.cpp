fe6227c135e98ad400a0c3ffe1a339742ed4e263
Denis Kulandin <kulandin08@gmail.com>
2020-07-16 13:30:34 +0300

    Ford-Falkerson max flow

---
+++ Flows/Ford_Falkerson.cpp
@@ -0,0 +1,152 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#pragma optimize("jillzay, jillzay, paci, paci, gilzi, gilzi")
+#pragma optimize("JARU SOSISONI")
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template <class T>
+istream& operator >> (istream& in, vector<T>& v){ for(auto& e : v){ in >> e;} return in;}
+template <class T>
+istream& operator >> (istream& in, pair<T, T> &v){ in >> v.first >> v.second; return in;}
+
+//#define int long long
+//#define int short int
+#define pb push_back
+#define mkp make_pair
+#define X first
+#define Y second
+#define cont continue
+#define For(i, a, b) for (int i = a; i < b;i++)
+#define forn(i, a) for (int i = 0;i < a;i++)
+#define ford(i, a, b) for (int i = b - 1;i >= a;i--)
+#define fordn(i, a) for (int i = a - 1;i >= 0;i--)
+#define forr(x, arr) for(auto &x: arr)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define makeunique(x) sort(all(x)), (x).resize(unique(all(x)) - (x).begin())
+void debug_out(){ cout << '\n';}
+
+template<typename head, typename... tail>
+void debug_out(head H, tail... T){ cout << " " << H; debug_out(T...);}
+
+#ifdef KULA
+    #define deb(...) cout << "[" << #__VA_ARGS__ << "] :", debug_out(__VA_ARGS__)
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const vector<T>& v){ out << "[";for(int i = 0;i < v.size();i++){out << v[i];if (i != v.size() - 1) out << " ";} out << "]"; return out; }
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const pair<T, T>& v){ out << "[";out << v.first << ", " << v.second;out << "]"; return out;}
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const set<T>& v){out << "["; for(auto i = v.begin();i != v.end();i++){ out << *i; if (*i != *v.rbegin()) out << ", ";} out << "]"; return out;}
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const multiset<T>& v){out << "["; for(auto i = v.begin();i != v.end();i++){ out << *i; if (*i != *v.rbegin()) out << ", ";} out << "]"; return out;}
+#else
+    #define deb(...)
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const vector<T>& v){ for(auto& e : v){ out << e << d;} return out;}
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const pair<T, T> & v){ out << v.X << d << v.Y; return out;}
+    template <char d = ' ', class T>
+    ostream& operator << (ostream& out, const set<T>& v){ for(auto& e : v){ out << e << d;} return out;}
+#endif
+typedef long long ll;
+typedef long double ldd;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef map<int, int> mii;
+typedef map<string, int> msi;
+typedef set<int> si;
+typedef set<pii> spii;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+template<class T> bool uin(T &a, T b) { return a > b ? (a = b, true) : false; }
+template<class T> bool uax(T &a, T b) { return a < b ? (a = b, true) : false; }
+template<typename T> T sqr(T a) {return a * a;}
+mt19937 gen(chrono::high_resolution_clock::now().time_since_epoch().count());
+const int dx[4] = {0, 1, 0, -1};
+const int dy[4] = {1, 0, -1, 0};
+//const int dx[8] = {0, 0, 1, 1, 1, -1, -1, -1};
+//const int dy[8] = {1, -1, 1, 0, -1, 1, 0, -1};
+//const int dx[8] = {-2, -2, 1, 1, -1, -1, 2, 2};
+//const int dy[8] = {-1, 1, -2, 2, -2, 2, -1, 1};
+const ldd EPS = 1e-12;
+const int MOD = 1e9 + 7;
+const ldd PI = acos(-1.);
+/*const int MAX_MEM = 1e8;
+int mpos = 0;
+char mem[MAX_MEM];
+inline void * operator new ( size_t n ) {
+    assert((mpos += n) <= MAX_MEM);
+  return (void *)(mem + mpos - n);
+}
+inline void operator delete ( void * ) noexcept { }*/
+
+struct edge{
+  int from, to, f, cap;
+};
+
+vector<edge> e;
+vvi g;
+vi used;
+int n, s, t, m;//s - исток, t - сток, n - кол-во вершин
+
+void add_edge(int x, int y, int c){
+  g[x].pb(e.size());
+  e.pb({x, y, 0, c});
+  g[y].pb(e.size());
+  e.pb({y, x, 0, 0});
+}
+
+int rem(int i){
+  return e[i].cap - e[i].f;
+}
+
+int dfs(int x, int mx){
+  if (x == t) return mx;
+  if (used[x] || mx == 0) return 0;
+  used[x] = 1;
+  forr(i, g[x]){
+    int to = e[i].to;
+    int fl = dfs(to, min(mx, rem(i)));
+    if (fl){
+      e[i].f += fl;
+      e[i ^ 1].f -= fl;
+      return fl;
+    }
+  }
+  return 0;
+}
+
+int max_flow(){
+  int ans = 0;
+
+  while(1){
+    used.assign(n, 0);
+    int f = dfs(s, INT_MAX);
+    if (!f) break;
+    ans += f;
+  }
+  return ans;
+}
+
+void solve(){
+
+}
+
+signed main() {
+  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+  #ifdef KULA
+//            freopen("input.txt", "r", stdin);
+//            freopen("output.txt", "w", stdout);
+  #else
+//            freopen("flow.in", "r", stdin);
+//            freopen("flow.out", "w", stdout);
+  #endif
+  int t = 1;
+//  cin >> t;
+  forn(i, t) solve();
+  return 0;
+}
