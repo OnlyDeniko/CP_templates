4ed988c01917c218dcffa07d62fe74e3f2c6dba0
Denis Kulandin <kulandin08@gmail.com>
2021-09-15 23:07:01 +0300

    sqrt update

--- sqrt_decomposition/sqrt_decomposition.cpp
+++ sqrt_decomposition/sqrt_decomposition.cpp
@@ -1,83 +1,23 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-//#pragma GCC optimize("Ofast,no-stack-protector")
-//#pragma GCC target("avx")
-
-using namespace __gnu_pbds;
-using namespace std;
-
-#define FUNC(name) template <ostream& out = cout, class... T> void name(T&&...){ }
-FUNC(prints) FUNC(printe) FUNC(printw) FUNC(printew) FUNC(printb) FUNC(printd) FUNC(printde);
-
-template <class T>
-istream& operator >> (istream& in, vector<T>& v){ for(auto& e : v){ in >> e; } return in; }
-template <char d = ' ', class T>
-ostream& operator << (ostream& out, vector<T>& v){ for(auto& e : v){ out << e << d; } return out; }
-template <char d = ' ', class T>
-ostream& operator << (ostream& out, set<T>& v){ for(auto& e : v){ out << e << d; } return out; }
-
-#define pb push_back
-#define mk make_pair
-#define X first
-#define Y second
-#define cont continue
-#define ret return
-#define For(i, a, b) for (int i = (int)a; i < (int)b;i++)
-#define forn(i, a) for (int i = 0;i < (int)a;i++)
-#define ford(i, a, b) for (int i = (int)b - 1;i >= (int)a;i--)
-#define fordn(i, a) for (int i = (int)a - 1;i >= 0;i--)
-#define forr(x, arr) for(auto &x: arr)
-#define all(x) x.begin(),x.end()
-#define REE(s_) {cout << s_ << '\n';return 0;}
-#define endl '\n'
-#define makeunique(x) sort(all(x)), (x).resize(unique(all(a)) - (x).begin())
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef double dd;
-typedef long double ldd;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<string> vs;
-typedef vector<pii> vpi;
-typedef vector<vi> vvi;
-typedef vector<ll> vll;
-typedef vector<vll> vvll;
-typedef pair<ll, ll> pll;
-typedef vector<pll> vpll;
-typedef map<int, int> mii;
-typedef map<string, int> msi;
-typedef set<int> si;
-typedef set<ll> sll;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_tree;
-
-template<class T> bool uin(T &a, T b) { return a > b ? (a = b, true) : false; }
-template<class T> bool uax(T &a, T b) { return a < b ? (a = b, true) : false; }
-
-//#define int long long
-
 struct decomp{
     int gr = 256;
     int n;
-    vi a, val, add, minn;
-    vll sum;
+    vector<int> a, val, add, minn;
+    vector<int> sum;
 
-    decomp(int n)
-        :n(n)
-        , a(n)
+    decomp(const vector<int> & aa)
+        :n(aa.size())
+        , a(aa)
         , val(1 + (n-1) / gr, INT_MAX)
         , add(1 + (n- 1) / gr, 0)
         , minn(1 + (n- 1) / gr, INT_MAX)
         , sum(1 + (n- 1) / gr, 0)
     {
-        For(i, 0, n){
-            cin >> a[i];
-            uin(minn[i / gr], a[i]);
+        for(int i = 0;i < n;i++){
+            minn[i / gr] = min(a[i], minn[i / gr]);
             sum[i / gr] += a[i];
         }
     }
     int grst(int g){
-
         return g * gr;
     }
     int get(int pos){
@@ -95,19 +35,17 @@ struct decomp{
         if (val[g] != INT_MAX){
             minn[g] = INT_MAX;
             sum[g] = 0;
-            For(i, grst(g), aft){
+            for(int i = grst(g);i < aft;i++){
                 a[i] = val[g];
             }
             val[g] = INT_MAX;
             add[g] = 0;
             minn[g] = INT_MAX;
             sum[g] = 0;
-
         }
         else if (add[g] != 0){
-            For(i, grst(g), aft){
+            for(int i = grst(g);i < aft;i++){
                 a[i] += add[g];
-
             }
             add[g] = 0;
         }
@@ -117,8 +55,8 @@ struct decomp{
         int aft = afterG(g);
         minn[g] = INT_MAX;
         sum[g] = 0;
-        For(i, grst(g), aft){
-            uin(minn[g], a[i]);
+        for(int i = grst(g);i < aft;i++){
+            minn[g] = min(minn[g], a[i]);
             sum[g] += a[i];
         }
     }
@@ -127,7 +65,7 @@ struct decomp{
         int rg = r / gr;
         if (lg == rg){
             norm(lg);
-            For(i, l, r + 1){
+            for(int i = l;i < r + 1;i++){
                 a[i] = x;
             }
             calc(lg);
@@ -135,26 +73,26 @@ struct decomp{
         else {
             norm(lg);
             int afterL = afterG(lg);
-            For(i, l, afterL){
+            for(int i = l;i < afterL;i++){
                 a[i] = x;
             }
             calc(lg);
-            For(g, lg + 1, rg){
+            for(int g = lg + 1;g < rg;g++){
                 val[g] = x;
             }
             norm(rg);
-            For(i, grst(rg), r + 1){
+            for(int i = grst(rg);i < r + 1;i++){
                 a[i] = x;
             }
             calc(rg);
         }
     }
-    void ad(int l, int r, int x){
+    void add(int l, int r, int x){
         int lg = l / gr;
         int rg = r / gr;
         if (lg == rg){
             norm(lg);
-            For(i, l, r + 1){
+            for(int i = l;i < r + 1;i++){
                 a[i] += x;
             }
             calc(lg);
@@ -162,11 +100,11 @@ struct decomp{
         else {
             norm(lg);
             int afterL = afterG(lg);
-            For(i, l, afterL){
+            for(int i = l;i < afterL;i++){
                 a[i] += x;
             }
             calc(lg);
-            For(g, lg + 1, rg){
+            for(int g = lg + 1;g < rg;g++){
                 if (val[g] == INT_MAX){
                     add[g] += x;
                     minn[g] += x;
@@ -175,19 +113,19 @@ struct decomp{
                 else val[g] += x;
             }
             norm(rg);
-            For(i, grst(rg), r + 1){
+            for(int i = grst(rg);i < r + 1;i++){
                 a[i] += x;
             }
             calc(rg);
         }
     }
-    ll rsq(int l, int r){
-        ll summ = 0;
+    int rsq(int l, int r){
+        int summ = 0;
         int lg = l / gr;
         int rg = r / gr;
         if (lg == rg){
             norm(lg);
-            For(i, l, r + 1){
+            for(int i = l;i < r + 1;i++){
                 summ += a[i];
             }
             calc(lg);
@@ -195,19 +133,18 @@ struct decomp{
         else {
             norm(lg);
             int afterL = afterG(lg);
-            For(i, l, afterL){
+            for(int i = l;i < afterL;i++){
                 summ += a[i];
             }
             calc(lg);
-            For(g, lg + 1, rg){
+            for(int g = lg + 1;g < rg;g++){
                 if (val[g] == INT_MAX){
                     summ += sum[g];
-
                 }
                 else summ += 1LL * gr * val[g];
             }
             norm(rg);
-            For(i, grst(rg), r + 1){
+            for(int i = grst(rg);i < r + 1;i++){
                 summ += a[i];
             }
             calc(rg);
@@ -220,86 +157,30 @@ struct decomp{
         int rg = r / gr;
         if (lg == rg){
             norm(lg);
-            For(i, l, r + 1){
-                uin(mi, a[i]);
+            for(int i = l;i < r + 1;i++){
+                mi = min(mi, a[i]);
             }
             calc(lg);
         }
         else {
             norm(lg);
             int afterL = afterG(lg);
-            For(i, l, afterL){
-                uin(mi, a[i]);
+            for(int i = l;i < afterL;i++){
+                mi = min(mi, a[i]);
             }
             calc(lg);
-            For(g, lg + 1, rg){
+            for(int g = lg + 1;g < rg;g++){
                 if (val[g] == INT_MAX){
-                    uin(mi, minn[g]);
-
+                    mi = min(mi, minn[g]);
                 }
-                else uin(mi, val[g]);
+                else mi = min(mi, val[g]);
             }
             norm(rg);
-            For(i, grst(rg), r + 1){
-                uin(mi, a[i]);
+            for(int i = grst(rg);i < r + 1;i++){
+                mi = min(mi, a[i]);
             }
             calc(rg);
         }
         return mi;
     }
 };
-
-signed main(){
-    //ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    #ifdef KULA
-        //freopen("input.txt", "r", stdin);
-        //freopen("output.txt", "w", stdout);
-    #else
-//        freopen("green.in", "r", stdin);
-//        freopen("green.out", "w", stdout);
-    #endif
-    int n;
-    cin >> n;
-    decomp sd(n);
-    int q;
-    cin >> q;
-    while(q--){
-        string s;
-        cin >> s;
-        if (s == "get"){
-            int pos;
-            cin >> pos;
-            pos--;
-            cout << sd.get(pos) << '\n';
-        }
-        else if (s == "update"){
-            int l, r, x;
-            cin >> l >> r >> x;
-            l--;
-            r--;
-            sd.update(l, r, x);
-        }
-        else if (s == "add"){
-            int l, r, x;
-            cin >> l >> r >> x;
-            l--;
-            r--;
-            sd.ad(l, r, x);
-        }
-        else if (s == "rsq"){
-            int l, r;
-            cin >> l >> r;
-            l--;
-            r--;
-            cout << sd.rsq(l, r) << '\n';
-        }
-        else if (s == "rmq"){
-            int l, r;
-            cin >> l >> r;
-            l--;
-            r--;
-            cout << sd.rmq(l, r) << '\n';
-        }
-    }
-    return 0;
-}
