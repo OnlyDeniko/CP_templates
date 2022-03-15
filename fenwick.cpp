2a9cf769e9ec6f0a95548a73c1373ad989ddd100
OnlyDeniko <kulandin08@gmail.com>
2022-03-15 12:14:45 +0300

    fenwick done

--- fenwick.cpp
+++ fenwick.cpp
@@ -1,39 +1,45 @@
-const int N = 1e6;
-int f[N];
+struct Fenwick{
+  int N;
+  vector<int> f;
 
-void add(int x, int y){
-  x++;
-  while(x < N){
-    f[x] += y;
-    x += x & -x;
+  Fenwick(int _n) : N(_n){
+    f.resize(N, 0);
   }
-}
 
-int sum(int x){
-  x++;
-  int ans = 0;
-  while(x){
-    ans += f[x];
-    x -= x & -x;
+  void add(int x, int y){
+    x++;
+    while(x < N){
+      f[x] += y;
+      x += x & -x;
+    }
   }
-  return ans;
-}
 
-int sum(int l, int r){
-  return sum(r) - sum(l - 1);
-}
+  int sum(int x){
+    x++;
+    int ans = 0;
+    while(x){
+      ans += f[x];
+      x -= x & -x;
+    }
+    return ans;
+  }
 
-// calculating the k-th unit on a segment
-// initially calc_pos(step, N, k)
-// where step = max {i: 2^i <= N}
-int calc_pos(int step, int pos, int k){ 
-  if (step == 0) return pos;
-  if (pos - step >= N){
-    return calc_pos(step >> 1, pos - step, k);
+  int sum(int l, int r){
+    return sum(r) - sum(l - 1);
   }
-  int elem = f[pos - step];
-  if (elem >= k){
-    return calc_pos(step >> 1, pos - step, k);
+
+  // calculating the k-th unit on a segment
+  // initially calc_pos(step, N, k)
+  // where step = max {i: 2^i <= N}
+  int calc_pos(int step, int pos, int k){ 
+    if (step == 0) return pos;
+    if (pos - step >= N){
+      return calc_pos(step >> 1, pos - step, k);
+    }
+    int elem = f[pos - step];
+    if (elem >= k){
+      return calc_pos(step >> 1, pos - step, k);
+    }
+    return calc_pos(step >> 1, pos, k - elem);
   }
-  return calc_pos(step >> 1, pos, k - elem);
-}
+};
