a0ae15975e7b681d3c2f9398ed59ef654893340d
OnlyDeniko <kulandin08@gmail.com>
2022-03-15 12:49:11 +0300

    gcdex done

---
+++ gcdex.cpp
@@ -0,0 +1,14 @@
+int gcdex(int a, int b, int& x, int& y) {
+	int c, q, xp, yp;
+	x = yp = 1, y = xp = 0;
+	while (b != 0) {
+		q = a / b;
+		c = a - q * b;
+		a = b, b = c;
+		c = x - q * xp;
+		x = xp, xp = c;
+		c = y - q * yp;
+		y = yp, yp = c;
+	}
+	return a;
+}
