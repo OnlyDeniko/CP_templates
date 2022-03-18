590321bee1bc413317ffe712183ad8f14b4fd168
OnlyDeniko <kulandin08@gmail.com>
2022-03-18 10:17:34 +0300

    upd sufarray & pers

--- Algorithms/Persistent ST/1.cpp
+++ Algorithms/Persistent ST/1.cpp
@@ -1,60 +1,62 @@
+int op(int a, int b){...}
+int neutral = ...;
 struct node{
-    node * left;
-    node * right;
-    int mi;
-    node(){
-        left = right = nullptr;
-        mi = mod;
-    }
-    node(int kek){
-        left = right = nullptr;
-        mi = kek;
-    }
-    node(node* l, node *r){
-        left = l;
-        right = r;
-        mi = mod;
-    }
-    node(node *l, node *r, int kek){
-        left = l;
-        right = r;
-        mi = kek;
-    }
+  node * left;
+  node * right;
+  int val;
+  node(){
+    left = right = nullptr;
+    val = neutral;
+  }
+  node(int _val){
+    left = right = nullptr;
+    val = _val;
+  }
+  node(node* l, node *r){
+    left = l;
+    right = r;
+    val = neutral;
+  }
+  node(node *l, node *r, int _val){
+    left = l;
+    right = r;
+    val = _val;
+  }
 };
 typedef node* tree;
 vector<node *> versions;
 vi a;
+
 tree build(int l, int r){
-    if (l == r){
-        return new node(a[l]);
-    }
-    int m = (l + r) >> 1;
-    tree q = build(l, m);
-    tree w = build(m + 1, r);
-    return new node(q, w, min(q->mi, w->mi));
+  if (l == r) return new node(a[l]);
+  int m = (l + r) >> 1;
+  tree left = build(l, m);
+  tree right = build(m + 1, r);
+  return new node(left, right, op(left->val, right->val));
 }
 
 tree update(const tree &t, int l, int r, int pos, int val){
-    if (l == r) return new node(val);
-    tree q = t->left, w = t->right;
-    int m = (l + r) >> 1;
-    if (pos <= m){
-        if (!q) q = new node();
-        q = update(q, l, m, pos, val);
-    }
-    else {
-        if (!w) w = new node();
-        w = update(w, m + 1, r, pos, val);
-    }
-    return new node(q, w, (q ? q->mi : 0) + (w ? w->mi : 0));
+  if (l == r) return new node(val);
+  tree left = t->left, right = t->right;
+  int m = (l + r) >> 1;
+  if (pos <= m){
+    left = (left == nullptr ? new node() :
+            update(left, l, m, pos, val));
+  }
+  else {
+    right = (right == nullptr ? new node() :
+            update(right, m + 1, r, pos, val));
+  }
+  return new node(left, right,
+    op((q ? q->mi : neutral), (w ? w->mi : neutral)));
 }
 
 int get(tree t, int tl, int tr, int l, int r){
-    if (l > r) return 0;
-    if ((!t) || (l == tl && r == tr)){
-        return t ? t->mi : 0;
-    }
-    int m = (tl + tr) >> 1;
-    int ans =  get(t->left, tl, m, l, min(r, m)) + get(t->right, m + 1, tr, max(l, m + 1), r);
-    return ans;
+  if (t == nullptr || l > r) return neutral;
+  if (l == tl && r == tr){
+    return t->val;
+  }
+  int m = (tl + tr) >> 1;
+  return op(get(t->left, tl, m, l, min(r, m)) +
+    get(t->right, m + 1, tr, max(l, m + 1), r));
 }
--- Algorithms/Strings/suf_mas.cpp
+++ Algorithms/Strings/suf_mas.cpp
@@ -1,72 +1,118 @@
-vector<int> suf_mas(string s){
-  s += char(1);
-  int n = s.size();
-  vector<int> p(n), c(n);
-  vector<pair<int, int>> a(n);
-  for(int i = 0;i < n;i++){
-    a[i] = {s[i], i};
+struct SuffixArray{
+  vector<int> mas, c, lcp;
+  int N;
+  string S;
+  SuffixArray(string _s) : N(_s.size()), S(_s){
+    build_suf(_s);
+    build_lcp();
   }
-  sort(a.begin(), a.end());
-  for(int i = 0;i < n;i++) p[i] = a[i].second;
-  c[p[0]] = 0;
-  for(int i = 1;i < n;i++){
-    c[p[i]] = c[p[i - 1]] + !!(s[p[i]] != s[p[i - 1]]);
-  }
-  int step = 0;
-  vector<int> counter(n, 0);
-  vector<int> new_p(n);
-  vector<int> new_c(n);
-  while((1 << step) < n){
-    fill(all(counter), 0);
-    for(int i = 0;i < n;i++){
-      new_p[i] = p[i] - (1 << step);
-      if (new_p[i] < 0) new_p[i] += n;
-      counter[c[new_p[i]]]++;
-    }
-    partial_sum(all(counter), counter.begin());
-    for(int i = n - 1;i >= 0;i--){
-      p[--counter[c[new_p[i]]]] = new_p[i];
-    }
-    new_c[p[0]] = 0;
+  void build_suf(string s){
+    s += char(0);
+    int n = s.size();
+    mas.resize(n);
+    c.resize(n);
+    vi counter(n, 0);
+    vpi a;
+    for(int i = 0;i < n;i++) a.pb({s[i], i});
+    sort(all(a));
+    for(int i = 0;i < n;i++)mas[i] = a[i].second;
+    c[mas[0]] = 0;
     for(int i = 1;i < n;i++){
-      int index1 = (p[i] + (1 << step)) % n;
-      int index2 = (p[i - 1] + (1 << step)) % n;
-      new_c[p[i]] = new_c[p[i - 1]];
-      if (c[index1] != c[index2] ||
-          c[p[i]] != c[p[i - 1]]){
-        new_c[p[i]]++;
+      c[mas[i]] = c[mas[i - 1]] + !!(s[mas[i]] != s[mas[i - 1]]);
+    }
+    int step = 0;
+    vector<int> new_p(n);
+    vector<int> new_c(n);
+    while((1 << step) < n){
+      fill(all(counter), 0);
+      for(int i = 0;i < n;i++){
+        new_p[i] =(mas[i] - (1 << step) + n) % n;
+        counter[c[new_p[i]]]++;
       }
+      partial_sum(all(counter), counter.begin());
+      for(int i = n - 1;i >= 0;i--){
+        mas[--counter[c[new_p[i]]]] = new_p[i];
+      }
+      new_c[mas[0]] = 0;
+      for(int i = 1;i < n;i++){
+        int index1 = (mas[i] + (1 << step)) % n;
+        int index2 = (mas[i - 1] + (1 << step)) % n;
+        new_c[mas[i]] = new_c[mas[i - 1]];
+        if (c[index1] != c[index2] ||
+            c[mas[i]] != c[mas[i - 1]]){
+          new_c[mas[i]]++;
+        }
+      }
+      c = new_c;
+      step++;
     }
-    c = new_c;
-    step++;
+    mas.erase(mas.begin());
+    c.erase(c.begin());
   }
-  p.erase(p.begin());
-  return p;
-}
-
-vector<int> calc_LCP(const vector<int> & suf, const string & s){
-  int n = suf.size();
-  vector<int> a(n);
-  vector<int> lcp(n, 0);
-  for(int i = 0;i < n;i++) a[suf[i]] = i;
-
-  auto calc = [&](int index1, int index2, int pos){
-    if (index2 == n) return 0;
-    int ans = pos;
-    index1 = suf[index1] + pos;
-    index2 = suf[index2] + pos;
-    while(max(index1, index2) < n){
-      if (s[index1] != s[index2]) break;
-      ans++;
-      index1++;
-      index2++;
-    }
-    return ans;
-  };
  
-  lcp[a[0]] = calc(a[0], a[0] + 1, 0);
-  for(int i = 1;i < n;i++){
-    lcp[a[i]] = calc(a[i], a[i] + 1, max(0, lcp[a[i - 1]] - 1));
+  void build_lcp(){
+    vi a(N);
+    for(int i = 0;i < N;i++) a[mas[i]] = i;
+    lcp.resize(N);
+ 
+    auto calc = [&](int index1, int index2,
+                    int pos)->int{
+      if (index2 == N) return 0;
+      int ans = pos;
+      index1 = mas[index1] + pos;
+      index2 = mas[index2] + pos;
+      while(max(index1, index2) < N){
+        if (S[index1] != S[index2]) break;
+        ans++, index1++, index2++;
+      }
+      return ans;
+    };
+ 
+    lcp[a[0]] = calc(a[0], a[0] + 1, 0);
+    for(int i = 1;i < N;i++){
+      lcp[a[i]] = calc(a[i], a[i] + 1, max(0, lcp[a[i - 1]] - 1));
+    }
   }
-  return lcp;
-}
+ 
+  ll unique_strings(){
+    ll sum = 0;
+    forr(i, lcp) sum += i;
+    return 1ll * N * (N + 1) / 2 - sum;
+  }
+ 
+  pii find(const string & t){
+    int l = 0, r = N - 1;
+    pii ans = {-1, -1};
+    for(int i = 0;i < (int)t.size();i++){
+      int ll = l, rr = r;
+      int left_ans = -1;
+      while(ll <= rr){
+        int mid = (ll + rr) >> 1;
+        if (mas[mid] + i < N && S[mas[mid] + i] >= t[i]){
+          left_ans = mid;
+          rr = mid - 1;
+        } else ll = mid + 1;
+      }
+      if (left_ans == -1 ||
+          S[mas[left_ans] + i] > t[i]){
+        return ans;
+      }
+      ll = left_ans, rr = r;
+      int right_ans = -1;
+      while(ll <= rr){
+        int mid = (ll + rr) >> 1;
+        if (mas[mid] + i < N && S[mas[mid] + i] <= t[i]){
+          right_ans = mid;
+          ll = mid + 1;
+        } else rr = mid - 1;
+      }
+      if (right_ans == -1 ||
+          S[mas[right_ans] + i] < t[i]){
+        return ans;
+      }
+      l = left_ans;
+      r = right_ans;
+    }
+    return {l, r};
+  }
+};
