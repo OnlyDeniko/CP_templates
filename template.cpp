e7d9505572694e41faa99d55d86de6feeafa330a
OnlyDeniko <kulandin08@gmail.com>
2022-11-07 09:59:03 +0300

    fixed

--- template.cpp
+++ template.cpp
@@ -6,24 +6,22 @@ using namespace std;
 #define pb push_back
 #define X first
 #define Y second
-#define fr first
-#define sc second
 #define forn(i, a) for (int i = 0;i < a;i++)
 #define forr(x, arr) for(auto &x: arr)
 #define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
 
-typedef long long ll;
-typedef long double ldd;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<pii> vpi;
-typedef vector<vi> vvi;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vpi = vector<pii>;
+using vvi = vector<vi>;
 
 void solve(){} 
 
 signed main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0); cout.tie(0);
+  cin.tie(0) -> sync_with_stdio(0);
+  cout.tie(0);
   solve();
   return 0;
 }
